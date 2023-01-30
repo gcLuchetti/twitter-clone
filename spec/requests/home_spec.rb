10ab56679bcd68eeac989a5de4274891106a7400
Gustavo Luchetti <carvalho_luchetti@hotmail.com>
2023-01-30 18:55:53 -0300

    add home presentation

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -4,6 +4,13 @@ require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    describe 'success' do
+      subject(:get_home) { get(root_path) }
+
+      it 'access the page' do
+        get_home
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
