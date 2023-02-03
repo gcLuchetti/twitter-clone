8fea5de44e8f014965dc2ba59ac7e1e1d379cd26
Gustavo Luchetti <carvalho_luchetti@hotmail.com>
2023-02-03 15:44:16 -0300

    add tweet

---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../support/devise'
+
+RSpec.describe Tweet, type: :model do
+  it 'should belongs to user' do
+    should belong_to(:user)
+  end
+
+  describe 'failure' do
+    let(:user) { create(:user) }
+
+    it 'when more then 280 caracteres' do
+      t = Tweet.new
+      t.user = User.new
+    end
+  end
+end
