1edf46d1925d322883d218ff6e57e94f0a0e2768
Gustavo Luchetti <carvalho_luchetti@hotmail.com>
2023-02-03 15:43:37 -0300

    add turbo rails

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -1,1 +1,2 @@
 //= require jquery
+//= require turbo_rails
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
-externals: {
-  "@hotwired/turbo-rails"
-}
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails"
