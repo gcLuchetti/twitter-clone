2848ea0a99456c9058d5452d0277ec30b8e3b73a
Gustavo Luchetti <carvalho_luchetti@hotmail.com>
2023-02-07 18:31:13 -0300

    add tests to Tweet model

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,8 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  it 'should belongs to user' do
-    should belong_to(:user)
+  describe 'success' do
+    it 'should belongs to user' do
+      should belong_to(:user)
+    end
+
+    it 'should validate the presence' do
+      should validate_presence_of(:body)
+    end
+
+    it 'should validate the length of the body' do
+      should validate_length_of(:body).is_at_most(280)
+    end
   end
 
   describe 'failure' do
