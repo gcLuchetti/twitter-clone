35560584fc54e249c75ff4bfc276727aeeb20f73
gcLuchetti <carvalho_luchetti@hotmail.com>
2023-02-13 01:17:22 -0300

    add tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,20 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'success' do
     let(:user1) { create(:user) }
+    let(:user2) { create(:user, :confirm_user) }
 
     it 'should have many tweet' do
       should have_many(:tweets).dependent(:destroy)
     end
 
-    it 'is valid' do
+    it 'is valid and confirmed - without trait' do
       expect(user1).to be_valid
+      user1.confirm
+      expect(user1).to be_confirmed
+    end
+
+    it 'is valid and confirmed - with trait' do
+      expect(user2).to be_valid.and be_confirmed
     end
   end
 
@@ -20,6 +27,7 @@ RSpec.describe User, type: :model do
     let(:userSmallPassword) { create(:user, password: 123) }
     let(:userWithoutPassword) { create(:user, password: nil) }
     let(:userWithoutEmail) { create(:user, email: nil) }
+    let(:userunconfirmed) { create(:user) }
 
     it 'has a unique email' do
       expect { userInvalidEmail }.to raise_error(ActiveRecord::RecordInvalid)
@@ -35,6 +43,10 @@ RSpec.describe User, type: :model do
 
     it 'is not valid without an email' do
       expect { userWithoutEmail }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'is not confirmed' do
+      expect(userunconfirmed).to_not be(userunconfirmed.confirmed?)
     end
   end
 end
