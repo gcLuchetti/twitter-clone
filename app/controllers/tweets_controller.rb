eb082b1f51bbc4addea39de312719b730a3e1db8
gcLuchetti <carvalho_luchetti@hotmail.com>
2023-02-17 09:50:44 -0300

    add before_action and change redirect path to dashboard

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     tweet = Tweet.new(tweet_params.merge(user: current_user))
     return unless tweet.save
 
-    redirect_to root_path
+    redirect_to dashboard_path
   end
 
   private
